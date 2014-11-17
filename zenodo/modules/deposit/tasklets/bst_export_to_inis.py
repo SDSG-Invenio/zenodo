30319172efbf0a9b3967d8e4be67bb36a2fa2db5
Jaime García <jaime.garcia.llopis@gmail.com>
2014-11-17 15:38:08 +0000

    deposit: WIP tasklet bst_export_to_inis moved to deposit module
    Signed-off-by: Jaime García <jaime.garcia.llopis@gmail.com>

--- zenodo/modules/deposit/tasklets/bst_export_to_inis.py
+++ zenodo/modules/deposit/tasklets/bst_export_to_inis.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 ##
 ## This file is part of Invenio.
-## Copyright (C) 2012 CERN.
+## Copyright (C) 2014 CERN.
 ##
 ## Invenio is free software; you can redistribute it and/or
 ## modify it under the terms of the GNU General Public License as
@@ -25,25 +25,24 @@ Export records to be consumed by INIS
 
 import os
 from datetime import datetime
-from tempfile import mkstemp
-from invenio.config import CFG_ETCDIR, CFG_TMPDIR
+from invenio.config import CFG_TMPDIR
 from invenio.legacy.dbquery import run_sql
-from invenio.legacy.search_engine import get_record, get_fieldvalues, perform_request_search
+from invenio.legacy.search_engine import perform_request_search
 from invenio.legacy.bibdocfile.api import BibRecDocs
 from invenio.legacy.bibsched.bibtask import write_message, \
-     task_sleep_now_if_required, \
-     task_update_progress
-from invenio.legacy.bibdocfile.managedocfiles import get_description_and_comment
-from invenio.legacy.bibdocfile.cli import cli_fix_marc
+    task_sleep_now_if_required, task_update_progress
 from invenio.legacy.bibsword.client import get_marcxml_from_record
+
 
 def find_all(a_str, sub):
     start = 0
     while True:
         start = a_str.find(sub, start)
-        if start == -1: return
+        if start == -1:
+            return
         yield start
         start += len(sub)
+
 
 def get_TRNs(recid):
     TRNs = []
@@ -52,12 +51,13 @@ def get_TRNs(recid):
         bibdoc = bibarchive.get_bibdoc(name)
         for bibdocfile in bibdoc.list_latest_files():
             if bibdocfile.format == '.ttf':
-		f = open(bibdocfile.get_path(), 'r')
-		ttf = f.read()
-		f.close()
-		TRNs += [ttf[i+4:i+13] for i in list(find_all(ttf, '001^'))]
+                f = open(bibdocfile.get_path(), 'r')
+                ttf = f.read()
+                f.close()
+                TRNs += [ttf[i+4:i+13] for i in list(find_all(ttf, '001^'))]
     return TRNs
-	        
+
+
 def get_fulltext_names(recid):
     names = []
     bibarchive = BibRecDocs(recid)
@@ -65,13 +65,15 @@ def get_fulltext_names(recid):
         bibdoc = bibarchive.get_bibdoc(name)
         for bibdocfile in bibdoc.list_latest_files():
             if bibdocfile.format == '.pdf':
-		names.append(name)
+                names.append(name)
     return names
 
+
 def all_file_names_in_TRNs(recid):
-    TRNs  = set(get_TRNs(recid))
+    TRNs = set(get_TRNs(recid))
     names = set(get_fulltext_names(recid))
     return names - TRNs == set()
+
 
 def get_last_export_date():
     """
@@ -89,6 +91,7 @@ def get_last_export_date():
 
     return last_export_date
 
+
 def store_last_export_date():
     """
     Stores the last date in which the exporting
@@ -100,6 +103,7 @@ def store_last_export_date():
     f.write(today)
     f.close()
 
+
 def get_records_to_check(recids, colls, last_checking_date):
     """
     Gets the recids of those records to be exported.
@@ -109,12 +113,12 @@ def get_records_to_check(recids, colls, last_checking_date):
     """
 
     if not recids and not colls:
-        recids_to_check = run_sql("select id from bibrec where modification_date > %s", \
+        recids_to_check = run_sql("select id from bibrec where modification_date > %s",
                                   (last_checking_date, ))
-        recids_to_check = [ result[0] for result in recids_to_check ]
+        recids_to_check = [result[0] for result in recids_to_check]
 
     if recids:
-	recid_list = []
+        recid_list = []
         cli_recid_list = recids.strip().split(',')
         for recid in cli_recid_list:
             if recid.find('-') > 0:
@@ -122,11 +126,10 @@ def get_records_to_check(recids, colls, last_checking_date):
                 try:
                     recid_min = rec_range[0]
                     recid_max = rec_range[1]
-		    for rec in perform_request_search(p="recid:"+recid_min+"->"+recid_max):
-		        recid_list.append(rec)
-                except Error, err:
-                    write_message("Error: [%s] occured while trying \
-                          to parse the recids argument." %err, sys.stderr)
+                    for rec in perform_request_search(p="recid:"+recid_min+"->"+recid_max):
+                        recid_list.append(rec)
+                except:
+                    write_message("Error while trying to parse the recids argument.")
                     return False
             else:
                 recid_list.append(int(recid))
@@ -137,6 +140,7 @@ def get_records_to_check(recids, colls, last_checking_date):
         recids_to_check = perform_request_search(c=cli_coll_list, )
 
     return recids_to_check
+
 
 def bst_export_to_inis(recids="", colls="", directory=None, force=0):
     """
@@ -150,18 +154,18 @@ def bst_export_to_inis(recids="", colls="", directory=None, force=0):
     @type force: int
     """
     force = int(force) and True or False
-    if directory == None:
-	directory = '/shared/'
+    if directory is None:
+        directory = '/shared/'
     elif directory[-1] != '/':
-	directory = directory + '/'
- 
+        directory = directory + '/'
+
     recids_to_check = get_records_to_check(recids, colls, get_last_export_date())
 
     i = 0
     out = ''
 
     for recid in recids_to_check:
-	if all_file_names_in_TRNs(recid):
+        if all_file_names_in_TRNs(recid):
             i += 1
             try:
                 bibarchive = BibRecDocs(recid)
@@ -185,8 +189,8 @@ def bst_export_to_inis(recids="", colls="", directory=None, force=0):
 #            get_description_and_comment(bibarchive.get_bibdoc(docname).list_latest_files())
 #
 #            # List all files that are not icons or subformats
-                current_files = [bibdocfile.get_path() for bibdocfile in bibdoc.list_latest_files() if \
-                             not bibdocfile.get_subformat() and not bibdocfile.is_icon()]
+                current_files = [bibdocfile.get_path() for bibdocfile in bibdoc.list_latest_files() if
+                                 not bibdocfile.get_subformat() and not bibdocfile.is_icon()]
 
                 ## current_files = []
                 ## if not force:
@@ -195,11 +199,11 @@ def bst_export_to_inis(recids="", colls="", directory=None, force=0):
                     out += current_filepath + '\n'
 
         else:
-	    write_message("pdfs and TRN don't match in " + str(recid))
+            write_message("pdfs and TRN don't match in " + str(recid))
 
-	f = open(directory + str(recid) + '.xml', 'w')
-	f.write(out + get_marcxml_from_record(recid))
-	f.close()
+        f = open(directory + str(recid) + '.xml', 'w')
+        f.write(out + get_marcxml_from_record(recid))
+        f.close()
 
     store_last_export_date()
     write_message("All records successfully exported")
